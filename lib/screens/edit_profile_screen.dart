0821f5e9cea5f4951a4f50c0313eff7f9c713fa0
samzylaw49-beep <samzylaw49@gmail.com>
2026-08-02 23:10:52 +0100

    Update edit_profile_screen.dart

--- lib/screens/edit_profile_screen.dart
+++ lib/screens/edit_profile_screen.dart
@@ -19,13 +19,9 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
 
 
   late TextEditingController usernameController;
-
   late TextEditingController displayNameController;
-
   late TextEditingController bioController;
-
   late TextEditingController locationController;
-
   late TextEditingController websiteController;
 
 
@@ -35,90 +31,37 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
 
     super.initState();
 
-
     usernameController =
-        TextEditingController(
-          text: currentUser.username,
-        );
-
+        TextEditingController(text: currentUser.username);
 
     displayNameController =
-        TextEditingController(
-          text: currentUser.displayName,
-        );
-
+        TextEditingController(text: currentUser.displayName);
 
     bioController =
-        TextEditingController(
-          text: currentUser.bio,
-        );
-
+        TextEditingController(text: currentUser.bio);
 
     locationController =
-        TextEditingController(
-          text: currentUser.location,
-        );
-
+        TextEditingController(text: currentUser.location);
 
     websiteController =
-        TextEditingController(
-          text: currentUser.website,
-        );
+        TextEditingController(text: currentUser.website);
 
   }
-
-
-
-
-  @override
-  void dispose() {
-
-    usernameController.dispose();
-
-    displayNameController.dispose();
-
-    bioController.dispose();
-
-    locationController.dispose();
-
-    websiteController.dispose();
-
-    super.dispose();
-
-  }
-
 
 
 
   void saveProfile() {
 
-
-    currentUser.username =
-        usernameController.text;
-
-
-    currentUser.displayName =
-        displayNameController.text;
+    currentUser.username = usernameController.text;
+    currentUser.displayName = displayNameController.text;
+    currentUser.bio = bioController.text;
+    currentUser.location = locationController.text;
+    currentUser.website = websiteController.text;
 
 
-    currentUser.bio =
-        bioController.text;
-
-
-    currentUser.location =
-        locationController.text;
-
-
-    currentUser.website =
-        websiteController.text;
-
-
-
-    Navigator.pop(context);
-
+    Navigator.pop(context, true);
 
   }
-
 
 
 
@@ -128,98 +71,66 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
 
     return Scaffold(
 
-
       appBar: AppBar(
-
-        title: const Text(
-          "Edit Profile",
-        ),
-
+        title: const Text("Edit Profile"),
       ),
-
 
 
       body: Padding(
 
-
         padding: const EdgeInsets.all(20),
 
-
-
         child: ListView(
-
 
           children: [
 
 
             TextField(
-
-              controller: usernameController,
-
-              decoration: const InputDecoration(
-                labelText: "Username",
-              ),
-
-            ),
-
-
-
-            TextField(
-
               controller: displayNameController,
-
               decoration: const InputDecoration(
                 labelText: "Display Name",
               ),
-
             ),
 
 
+            TextField(
+              controller: usernameController,
+              decoration: const InputDecoration(
+                labelText: "Username",
+              ),
+            ),
+
 
             TextField(
-
               controller: bioController,
-
               decoration: const InputDecoration(
                 labelText: "Bio",
               ),
-
             ),
 
 
-
             TextField(
-
               controller: locationController,
-
               decoration: const InputDecoration(
                 labelText: "Location",
               ),
-
             ),
-
 
 
             TextField(
-
               controller: websiteController,
-
               decoration: const InputDecoration(
                 labelText: "Website",
               ),
-
             ),
-
 
 
             const SizedBox(height: 25),
 
 
-
             ElevatedButton(
 
               onPressed: saveProfile,
-
 
               child: const Text(
                 "Save Changes",
@@ -227,18 +138,13 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
 
             ),
 
-
           ],
-
 
         ),
 
-
       ),
 
-
     );
-
 
   }
 
