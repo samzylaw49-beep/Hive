1588fbd3bc38cacd401a26cfe366586d12890e0f
samzylaw49-beep <samzylaw49@gmail.com>
2026-08-02 23:43:01 +0100

    Update edit_profile_screen.dart

--- lib/screens/edit_profile_screen.dart
+++ lib/screens/edit_profile_screen.dart
@@ -1,5 +1,6 @@
  import 'package:flutter/material.dart';
 import 'user_data.dart';
+import 'home_screen.dart';
 
 
 class EditProfileScreen extends StatefulWidget {
@@ -31,37 +32,99 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
 
     super.initState();
 
+
     usernameController =
-        TextEditingController(text: currentUser.username);
+        TextEditingController(
+          text: currentUser.username,
+        );
+
 
     displayNameController =
-        TextEditingController(text: currentUser.displayName);
+        TextEditingController(
+          text: currentUser.displayName,
+        );
+
 
     bioController =
-        TextEditingController(text: currentUser.bio);
+        TextEditingController(
+          text: currentUser.bio,
+        );
+
 
     locationController =
-        TextEditingController(text: currentUser.location);
+        TextEditingController(
+          text: currentUser.location,
+        );
+
 
     websiteController =
-        TextEditingController(text: currentUser.website);
+        TextEditingController(
+          text: currentUser.website,
+        );
+
 
   }
 
 
 
-  void saveProfile() {
 
-    currentUser.username = usernameController.text;
-    currentUser.displayName = displayNameController.text;
-    currentUser.bio = bioController.text;
-    currentUser.location = locationController.text;
-    currentUser.website = websiteController.text;
+  @override
+  void dispose() {
 
+    usernameController.dispose();
+    displayNameController.dispose();
+    bioController.dispose();
+    locationController.dispose();
+    websiteController.dispose();
 
-    Navigator.pop(context, true);
+    super.dispose();
 
   }
+
+
+
+
+
+  void finishSetup() {
+
+
+    currentUser.username =
+        usernameController.text;
+
+
+    currentUser.displayName =
+        displayNameController.text;
+
+
+    currentUser.bio =
+        bioController.text;
+
+
+    currentUser.location =
+        locationController.text;
+
+
+    currentUser.website =
+        websiteController.text;
+
+
+
+    Navigator.pushReplacement(
+
+      context,
+
+      MaterialPageRoute(
+
+        builder: (context) =>
+            const HomeScreen(),
+
+      ),
+
+    );
+
+
+  }
+
 
 
 
@@ -71,80 +134,167 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
 
     return Scaffold(
 
+
       appBar: AppBar(
-        title: const Text("Edit Profile"),
+
+        title:
+            const Text("Setup Profile"),
+
       ),
+
+
 
 
       body: Padding(
 
-        padding: const EdgeInsets.all(20),
+
+        padding:
+            const EdgeInsets.all(20),
+
+
 
         child: ListView(
+
 
           children: [
 
 
+
             TextField(
-              controller: displayNameController,
-              decoration: const InputDecoration(
-                labelText: "Display Name",
+
+              controller:
+                  displayNameController,
+
+              decoration:
+                  const InputDecoration(
+
+                labelText:
+                    "Display Name",
+
               ),
+
             ),
 
 
-            TextField(
-              controller: usernameController,
-              decoration: const InputDecoration(
-                labelText: "Username",
-              ),
-            ),
-
-
-            TextField(
-              controller: bioController,
-              decoration: const InputDecoration(
-                labelText: "Bio",
-              ),
-            ),
 
 
             TextField(
-              controller: locationController,
-              decoration: const InputDecoration(
-                labelText: "Location",
+
+              controller:
+                  usernameController,
+
+              decoration:
+                  const InputDecoration(
+
+                labelText:
+                    "Username",
+
               ),
+
             ),
+
+
 
 
             TextField(
-              controller: websiteController,
-              decoration: const InputDecoration(
-                labelText: "Website",
-              ),
-            ),
 
+              controller:
+                  bioController,
 
-            const SizedBox(height: 25),
+              decoration:
+                  const InputDecoration(
 
+                labelText:
+                    "Bio",
 
-            ElevatedButton(
-
-              onPressed: saveProfile,
-
-              child: const Text(
-                "Save Changes",
               ),
 
             ),
+
+
+
+
+            TextField(
+
+              controller:
+                  locationController,
+
+              decoration:
+                  const InputDecoration(
+
+                labelText:
+                    "Location",
+
+              ),
+
+            ),
+
+
+
+
+            TextField(
+
+              controller:
+                  websiteController,
+
+              decoration:
+                  const InputDecoration(
+
+                labelText:
+                    "Website",
+
+              ),
+
+            ),
+
+
+
+
+            const SizedBox(height: 30),
+
+
+
+
+            SizedBox(
+
+              width:
+                  double.infinity,
+
+
+
+              child: ElevatedButton(
+
+
+
+                onPressed:
+                    finishSetup,
+
+
+
+                child:
+                    const Text(
+                      "Finish Setup",
+                    ),
+
+
+
+              ),
+
+            ),
+
+
 
           ],
 
+
         ),
+
 
       ),
 
+
     );
+
 
   }
 
