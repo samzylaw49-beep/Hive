07e5d6d6e869426f4ff5405d8576ece5e38eff7f
samzylaw49-beep <samzylaw49@gmail.com>
2026-08-02 23:06:30 +0100

    Create user_data.dart

---
+++ lib/screens/user_data.dart
@@ -0,0 +1,16 @@
+import 'user_model.dart';
+
+
+UserProfile currentUser = UserProfile(
+
+  username: "hive_user",
+
+  displayName: "Hive User",
+
+  bio: "Welcome to Hive 🐝",
+
+  location: "",
+
+  website: "",
+
+);
