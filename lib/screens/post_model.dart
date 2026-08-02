2c85a2d5b9bde1efcbc93699aada50c51e14d6eb
samzylaw49-beep <samzylaw49@gmail.com>
2026-08-02 22:55:13 +0100

    Create post_model.dart

---
+++ lib/screens/post_model.dart
@@ -0,0 +1,11 @@
+class Post {
+  final String username;
+  final String content;
+  final DateTime createdAt;
+
+  Post({
+    required this.username,
+    required this.content,
+    required this.createdAt,
+  });
+}
