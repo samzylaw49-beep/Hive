d7c2ab2e347ead4cc9d40467ded60e8a4eac7074
samzylaw49-beep <samzylaw49@gmail.com>
2026-08-03 11:05:55 +0100

    Update post_model.dart

--- lib/screens/post_model.dart
+++ lib/screens/post_model.dart
@@ -1,11 +1,13 @@
-class Post {
+ class Post {
   final String username;
   final String content;
   final DateTime createdAt;
+  int likes;
 
   Post({
     required this.username,
     required this.content,
     required this.createdAt,
+    this.likes = 0,
   });
 }
