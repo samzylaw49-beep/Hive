8ec60720c5dc24cf77718948ba20f48444adfeb6
samzylaw49-beep <samzylaw49@gmail.com>
2026-08-03 11:32:28 +0100

    Update post_model.dart

--- lib/screens/post_model.dart
+++ lib/screens/post_model.dart
@@ -4,7 +4,10 @@ class Post {
   final String username;
   final String content;
   final DateTime createdAt;
+
   int likes;
+  int reposts;
+
   final List<Comment> comments;
 
   Post({
@@ -12,6 +15,7 @@ class Post {
     required this.content,
     required this.createdAt,
     this.likes = 0,
+    this.reposts = 0,
     this.comments = const [],
   });
 }
